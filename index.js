1a3095a4f71f6ac6837ef96d4ecfd78f0c589bcb
Khairul38 <khairulislam38@gmail.com>
2021-10-31 17:27:47 +0600

    server

--- index.js
+++ index.js
@@ -1,8 +1,67 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qdveb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('trabeloky');
+        const packageCollection = database.collection('packages')
+        const destinationCollection = database.collection('destinations');
+        const bookingCollection = database.collection('bookings');
+
+
+        // Get Packages API
+        app.get('/packages', async (req, res) => {
+            const cursor = packageCollection.find({});
+            const packages = await cursor.toArray();
+            res.send(packages)
+        });
+        // Get Booking API
+        app.get('/bookings', async (req, res) => {
+            const cursor = bookingCollection.find({})
+            const bookings = await cursor.toArray();
+            res.send(bookings);
+        })
+        // Get Destination API
+        app.get('/destinations', async (req, res) => {
+            const cursor = destinationCollection.find({});
+            const destinations = await cursor.toArray();
+            res.send(destinations);
+        });
+
+        // Post Packages API
+        app.post('/packages', async (req, res) => {
+            const package = req.body;
+            const result = await packageCollection.insertOne(package);
+            res.json(result)
+        });
+
+        // Post Bookings API
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            const result = await bookingCollection.insertOne(booking);
+            res.json(result);
+        })
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
